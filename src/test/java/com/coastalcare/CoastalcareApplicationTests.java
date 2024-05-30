e16a4ba339e73e4957ba631f776078029d49d6be
diogo_gallina <diogogallina20@gmail.com>
2024-05-29 21:02:29 -0300

    feat: add root project

---
+++ src/test/java/com/coastalcare/CoastalcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coastalcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoastalcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
